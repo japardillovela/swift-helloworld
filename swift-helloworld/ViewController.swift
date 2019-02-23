e5460d695dba7a472d51a22e8c450398c2d5bd39
Jose Angel Pardillo Vela <ja.pv@icloud.com>
2019-02-23 07:04:30 +0000

    Initial Commit

---
+++ swift-helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-helloworld
+//
+//  Created by Jose Angel Pardillo Vela on 23/02/2019.
+//  Copyright © 2019 Globile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
